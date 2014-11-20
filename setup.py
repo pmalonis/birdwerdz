179177499daf5f6d54c87f7d4925630391b3f9c4
Peter Malonis <pmalonis@uchicago.edu>
2014-11-20 13:47:31 -0600

    setup script now creates package with modules

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 #from distutils.command.build_ext import build_ext
@@ -8,10 +8,12 @@ import numpy
 
 setup(
     name = "birdwerdz",
+    entry_points = {'console_scripts':'birdwerdz=birdwerdz._main:main'},
+    packages = ['birdwerdz'],
     cmdclass = {"build_ext" : build_ext},
-    ext_modules = [Extension("birdwerdz",
-                             sources=["birdwerdz/birdwerdz.pyx", "birdwerdz/c_functions.c"],
+    ext_modules = [Extension("birdwerdz.dtw",
+                             sources=["birdwerdz/dtw.pyx", "birdwerdz/c_functions.c"],
                              include_dirs=['.', numpy.get_include()])],
-    scripts = ['classify.py', 'plot_cluster_means.py', 'select_cluster.py', 'labeling.py', 'cluster.py']
 )
+
 
